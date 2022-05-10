3e3754b709c0951d9c398116d67343d45c5b13ee
Tamil-selvan-tring <102953614+Tamil-selvan-tring@users.noreply.github.com>
2022-05-10 19:20:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Formbuild from './Formbuild';
+
+function App() {
+  return (
+    <Formbuild/>
+  );
+}
+
+export default App;
---
+++ src/Edit.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+function Edit()
+{
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(  
+        <React.StrictMode>
+            <h1>Hiii</h1>
+        </React.StrictMode>
+      )
+}   
+export default Edit;

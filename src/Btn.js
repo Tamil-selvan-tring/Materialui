cfaa4cc74ca15b617d1d11af908fa75bd6414067
Tamil-selvan-tring <102953614+Tamil-selvan-tring@users.noreply.github.com>
2022-05-11 20:47:38 +0300

    Delete src directory

--- src/Btn.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Handleclick()
-{
-}
-function Btn(props)
-{
-    return(<button onClick={Handleclick}>{props.name}</button>);
-}
-export default Btn;
--- src/CRUD.js
+++
@@ -1,7 +0,0 @@
-function del(e)
-{
-  let di=document.getElementById('card');
-  let chld=document.getElementById(e);
-  di.removeChild(chld);
-}
-export default del;
